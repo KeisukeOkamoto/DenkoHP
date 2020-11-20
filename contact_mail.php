53776f5b07f38114547df2444dd8376e2a72e66e
KeisukeOkamoto <73532961+KeisukeOkamoto@users.noreply.github.com>
2020-11-20 09:49:50 +0900

    メール機能の修正、Readmeの追加

--- contact_mail.php
+++ contact_mail.php
@@ -1,29 +1,11 @@
-
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-  <meta charset="utf-8">
-  <title>SEIKEI Denko</title>
-  <meta name="description" content="2020年度　成蹊大学電子工学研究部のホームページ">
-
-  <!-- CSS -->
-  <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Philosopher" rel="stylesheet">
-  <link href="css/style.css" rel="stylesheet">
-  <link rel="icon" type="image/png" href="images/favicon.jpg">
-</head>
-
-<body>
   <?php
-
   try
 {
-  $message = $_POST['message'];
   $to = "keislaim@gmail.com";
   $subject = $_POST['name'];
   $message = $_POST['message'];
-  $headers = "From:keislaim@outlook.jp";
+  $from = $_POST['email'];
+  $headers = "From:$from";
   mb_language("Japanese");
   mb_internal_encoding("UTF-8");
   if(mb_send_mail($to, $subject, $message, $headers)){
@@ -40,6 +22,3 @@ catch(Exception $e)
 }
 
 ?>
-</body>
-
-</html>
--- stripe/charge.php
+++ stripe/charge.php
@@ -1,6 +1,6 @@
 <?php
 // ライブラリを読み込む
-require_once ( dirname(__FILE__).'/lib/stripe-php-7.61.0/init.php');
+require_once ( './lib/stripe-php-7.61.0/init.php');
 
 // Secret Keyをセット
 $secret_key = 'sk_test_51HeULKG2ft5wb9WTKHyweFfx8lPmLSFSj0mV4BxBRZd0WJ5JAqrKCorV23LMcMedaTR3yADtubR64kEnaN6HgObv00pqDKk53X';
@@ -24,22 +24,8 @@ $charge = \Stripe\Charge::create(array(
 // 決済できなかったときの処理
 die('決済が完了しませんでした');
 }
-
-// 決済完了後にメルマガへ自動登録
-$post_url = 'https://*********/';
-$param = array(
-'Publisher_Id' => '****',
-'Magazine_No' => '**',
-'Mail' => $email,
-);
-$options = array('https' => array(
-'method' => 'POST',
-'content' => http_build_query($param),
-));
-file_get_contents($url, false, stream_context_create($options));
-
 // サンクスページへリダイレクト
-header('Location: thanks.html');
+header('Location: ../thanks.html');
 exit;
 }
 ?>
